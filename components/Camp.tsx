5a81705d6df49d69b93bdd85a2ed8043ef4a0fb1
David <davidagada05@gmail.com>
2024-06-19 12:36:56 -0600

    Finished hero section and starting camp section implimentation

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Camp = () => {
   return (
-    <div>Camp</div>
+    <section className = "border-2 border-green-500 2xl:max-container relative flex 
+    flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+      
+
+    </section>
   )
 }
 
